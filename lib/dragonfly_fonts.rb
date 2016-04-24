c84f476a387ca6f653b220da90270037af023e3b
Tomas Celizna <tomas.celizna@gmail.com>
2016-04-24 23:07:32 +0200

    do not auto-configure

--- lib/dragonfly_fonts.rb
+++ lib/dragonfly_fonts.rb
@@ -6,7 +6,3 @@ require 'dragonfly_fonts/version'
 module DragonflyFonts
   SCRIPT_DIR = Pathname.new(File.expand_path('../../script', __FILE__))
 end
-
-Dragonfly.app.configure do
-  plugin :fonts
-end
