30c66d2dc9faf55879a66a6bf3c4b2b640ca1d4a
Tomas Celizna <tomas.celizna@gmail.com>
2016-08-27 21:46:35 +0200

    fix encoding

--- test/dragonfly_fonts/processors/fix_dflt_table_test.rb
+++ test/dragonfly_fonts/processors/fix_dflt_table_test.rb
@@ -10,7 +10,7 @@ describe DragonflyFonts::Processors::FixDfltTable do
 
   describe 'invalid check' do
     it 'considers data with LangSysRecord after ScriptTag[value="DFLT"] invalid' do
-      asset.encode(:otf).ot_sanitise.force_encoding('utf-8').must_include "DFLT table doesn't satisfy the spec"
+      asset.encode(:otf).ot_sanitise.encode('UTF-8').must_include "DFLT table doesn't satisfy the spec"
     end
   end
 
