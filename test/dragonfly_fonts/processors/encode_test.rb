a65bb82685476d9968494013e760c5e1e7d6b742
Tomáš Celizna <tomas.celizna@gmail.com>
2018-05-27 21:32:30 +0200

    speed up tests

--- test/dragonfly_fonts/processors/encode_test.rb
+++ test/dragonfly_fonts/processors/encode_test.rb
@@ -7,18 +7,22 @@ describe DragonflyFonts::Processors::Encode do
     next unless File.exists?(SAMPLES_DIR.join("sample.#{format}"))
     describe format.to_s do
       let(:content) { app.fetch_file SAMPLES_DIR.join("sample.#{format}") }
-      it { content.encode('otf').ext.must_equal 'otf' }
-      it { content.encode('otf').mime_type.must_equal 'font/otf' }
-      it { content.encode('otf').size.must_be :>, 0 }
+      it do
+        content.encode('otf').ext.must_equal 'otf'
+        content.encode('otf').mime_type.must_equal 'font/otf'
+        content.encode('otf').size.must_be :>, 0
+      end
     end
   end
 
   DragonflyFonts::SUPPORTED_OUTPUT_FORMATS.each do |format|
     describe "output to #{format}" do
       let(:content) { app.fetch_file SAMPLES_DIR.join('sample.otf') }
-      it { content.encode(format).ext.must_equal format }
-      it { content.encode(format).mime_type.must_equal app.mime_types[".#{format}"] }
-      it { content.encode(format).size.must_be :>, 0 }
+      it do
+        content.encode(format).ext.must_equal format
+        content.encode(format).mime_type.must_equal app.mime_types[".#{format}"]
+        content.encode(format).size.must_be :>, 0
+      end
     end
   end
 end
