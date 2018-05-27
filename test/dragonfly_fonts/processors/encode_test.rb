55aa4ecb67bdf35ecedfa07e20b2501e00751d92
Tomáš Celizna <tomas.celizna@gmail.com>
2018-05-28 00:08:16 +0200

    test all variants

--- test/dragonfly_fonts/processors/encode_test.rb
+++ test/dragonfly_fonts/processors/encode_test.rb
@@ -3,30 +3,19 @@ require 'test_helper'
 describe DragonflyFonts::Processors::Encode do
   let(:app) { test_app.configure_with(:fonts) }
 
-  describe 'SUPPORTED_FORMATS' do
-    DragonflyFonts::SUPPORTED_FORMATS.each do |format|
-      unless File.exist?(SAMPLES_DIR.join("sample.#{format}"))
-        it(format) { skip "sample.#{format} does not exist, skipping" }
-        next
-      end
-
-      let(:content) { app.fetch_file SAMPLES_DIR.join("sample.#{format}") }
-      it(format) do
-        result = content.encode('otf')
-        result.ext.must_equal 'otf'
-        result.mime_type.must_equal 'font/otf'
-        result.size.must_be :>, 0
-      end
+  DragonflyFonts::SUPPORTED_FORMATS.each do |format|
+    unless File.exist?(SAMPLES_DIR.join("sample.#{format}"))
+      it(format) { skip "sample.#{format} does not exist, skipping" }
+      next
     end
-  end
 
-  describe 'SUPPORTED_OUTPUT_FORMATS' do
-    DragonflyFonts::SUPPORTED_OUTPUT_FORMATS.each do |format|
-      let(:content) { app.fetch_file SAMPLES_DIR.join('sample.otf') }
-      it(format) do
-        result = content.encode(format)
-        result.ext.must_equal format
-        result.mime_type.must_equal app.mime_types[".#{format}"]
+    let(:content) { app.fetch_file SAMPLES_DIR.join("sample.#{format}") }
+
+    DragonflyFonts::SUPPORTED_OUTPUT_FORMATS.each do |output_format|
+      it("#{format} to #{output_format}") do
+        result = content.encode(output_format)
+        result.ext.must_equal output_format
+        result.mime_type.must_equal app.mime_types[".#{output_format}"]
         result.size.must_be :>, 0
       end
     end
