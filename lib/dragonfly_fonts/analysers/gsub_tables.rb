5ba01f98336043f96710a8f9db3a4f54513168e3
Tomáš Celizna <tomas.celizna@gmail.com>
2018-07-07 09:06:42 +0200

    temp fix before capture3 is merged into dragonfly
    https://github.com/markevans/dragonfly/pull/488

--- lib/dragonfly_fonts/analysers/gsub_tables.rb
+++ lib/dragonfly_fonts/analysers/gsub_tables.rb
@@ -6,9 +6,14 @@ module DragonflyFonts
       def call(content)
         return [] unless FONT_FORGE_SUPPORTED_FORMATS.include?(content.ext)
 
-        details = content.shell_eval do |path|
-          "#{DragonflyFonts::SCRIPT_DIR.join('gsub_tables.py')} #{path}"
-        end
+        # details = content.shell_eval do |path|
+        #   "#{DragonflyFonts::SCRIPT_DIR.join('gsub_tables.py')} #{path}"
+        # end
+
+        command = "#{DragonflyFonts::SCRIPT_DIR.join('gsub_tables.py')} #{content.path}"
+        details, stderr_str, status = Open3.capture3(command)
+
+        return [] unless details.present?
 
         JSON.parse(details)
       end
